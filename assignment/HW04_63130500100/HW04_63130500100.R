537feea6c8be5a1c0deb3655b1180d07f2ad4bc0
Tttangentt <72536703+Tttangentt@users.noreply.github.com>
2021-10-25 17:36:06 +0700

    Update HW04_63130500100.R

--- assignment/HW04_63130500100/HW04_63130500100.R
+++ assignment/HW04_63130500100/HW04_63130500100.R
@@ -21,35 +21,35 @@ com_science %>% select(Rating,Book_title)
 str_sort(com_science$Rating)
 
 #Part3
-#1 ¤èÒà©ÅÕèÂ¢Í§ÃÒ¤ÒË¹Ñ§Ê×Í·Ñé§ËÁ´ áÅÐË¹Ñ§Ê×Í·ÕèÁÕÃÒ¤Òá¾§·ÕèÊØ´ª×èÍÍÐäÃ ÃÒ¤Òà·èÒäËÃè
+#1 ค่าเฉลี่ยของราคาหนังสือทั้งหมด และหนังสือที่มีราคาแพงที่สุดชื่ออะไร ราคาเท่าไหร่
 mean(com_science$Price)
 com_science %>% select(Book_title,Price) %>% filter(Price == max(Price))
 
-#2 Ë¹Ñ§Ê×Í·ÕèÁÕ Rating µèÓ¡ÇèÒ 4 ÁÕ¡ÕèàÅèÁ
+#2 หนังสือที่มี Rating ต่ำกว่า 4 มีกี่เล่ม
 lessthan4 <- filter(com_science,Rating < 4)
 count(lessthan4)
 
-#3 ÁÕË¹Ñ§Ê×Í»ÃÐàÀ·ÍÐäÃºéÒ§ áÅÐáµèÅÐ»ÃÐàÀ·ÁÕË¹Ñ§Ê×Í·Ñé§ËÁ´¡ÕèàÅèÁ
+#3 มีหนังสือประเภทอะไรบ้าง และแต่ละประเภทมีหนังสือทั้งหมดกี่เล่ม
 com_science %>% distinct(Type)
 amount_book <- com_science %>% count(com_science$Type)
 amount_book
 
-#4 Ë¹Ñ§Ê×Í·ÕèÁÕ¤¹ review à·èÒ¡Ñº 0 ÁÕ¡ÕèàÅèÁ àÅèÁäË¹ºéÒ§
+#4 หนังสือที่มีคน review เท่ากับ 0 มีกี่เล่ม เล่มไหนบ้าง
 review0 <- com_science %>% select(Book_title,Reviews) %>% filter(com_science$Reviews == 0)
 review0
 count(review0) %>% rename(amount = n)
 
-#5 ¤èÒà©ÅÕèÂ¨Ó¹Ç¹Ë¹éÒË¹Ñ§Ê×Í áÅÐË¹Ñ§Ê×Í·ÕèÁÕË¹éÒ¹éÍÂ·ÕèÊØ´¤×ÍÍÐäÃ
+#5 ค่าเฉลี่ยจำนวนหน้าหนังสือ และหนังสือที่มีหน้าน้อยที่สุดคืออะไร
 mean(com_science$Number_Of_Pages)
 com_science %>% select(Book_title,Number_Of_Pages)%>%filter(Number_Of_Pages == min(Number_Of_Pages))
 
-#6 Ë¹Ñ§Ê×Í·ÕèÁÕÃÒ¤ÒÁÒ¡¡ÇèÒÃÒ¤Òà©ÅÕèÂÁÕ·Ñé§ËÁ´¡ÕèàÅèÁ
+#6 หนังสือที่มีราคามากกว่าราคาเฉลี่ยมีทั้งหมดกี่เล่ม
 moreThan_mean <- com_science %>% select(Price) %>% filter(Price > mean(com_science$Price))
 count(moreThan_mean)
 
 #Part4
-#¡ÃÒ¿áÊ´§¨Ó¹Ç¹Ë¹Ñ§Ê×ÍáµèÅÐ Type
+#กราฟแสดงจำนวนหนังสือแต่ละ Type
 ggplot(com_science,aes(x=Type)) + geom_bar()
 
-#¡ÃÒ¿áÊ´§Ë¹Ñ§Ê×Í·ÕèÁÕ¨Ó¹Ç¹Ë¹éÒÁÒ¡¡ÇèÒ 1500 Ë¹éÒ
+#กราฟแสดงหนังสือที่มีจำนวนหน้ามากกว่า 1500 หน้า
 com_science %>% filter(Number_Of_Pages > 1500) %>% ggplot(aes(x=Book_title, y=Number_Of_Pages))+geom_point()+ggtitle("Amount of Page more than 1500")
