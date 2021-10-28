bf8531e1e92df5c20d194240b5007d9ec9e6ca8c
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-10-28 19:07:58 +0700

    Update 63130500120 work EIEIEIEIEIEIEIEI

--- assignment/HW04_63130500120/HW04_63130500120.R
+++ assignment/HW04_63130500120/HW04_63130500120.R
@@ -1,1 +1,41 @@
-#First Commit
+#install lib
+install.packages("dplyr")
+install.packages("readr")
+install.packages("ggplot2")
+
+#load lib
+library(dplyr)
+library(readr)
+library(ggplot2)
+
+#import datasets
+top_cs <- read_csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/prog_book.csv")
+
+#Explore dataset
+glimpse(top_cs)
+
+# Part 2
+# 1
+top_cs %>%
+  filter(Type == 'Hardcover') %>%
+  filter(Rating == max(Rating)) %>%
+  select(Book_title,Rating)
+
+# 2
+top_cs %>% 
+  slice(1:10) %>%
+  select(Book_title)
+
+# 3
+top_cs %>%
+  mutate(Rating/Reviews)
+
+# 4
+top_cs %>% 
+  arrange(Price) %>%
+  slice(1:5)
+
+# 5 
+top_cs %>% relocate(Book_title)
+
+# Part 3
